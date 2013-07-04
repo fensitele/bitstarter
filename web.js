6e715782346eade3efa896c064af80ac737b825e
JIN QI <king.qi@gmail.com>
2013-07-04 06:56:09 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var buff = buf.toString(fs.readFileSync('index.html')
+  response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
